d6ad986fc6d5bc6a42042b2a2ae7ffa596f762e1
ecaterina.hrib <ecaterina.hrib@gmail.com>
2024-01-08 03:44:17 +0200

    upload faulty added

--- AI-module/main.py
+++ AI-module/main.py
@@ -1,83 +1,26 @@
-import os
-import uvicorn
+import sys
+
 import requests
-import xml.etree.ElementTree as ET
+import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
-from urllib.parse import urlencode
-
 from dotenv import load_dotenv
+
+from models.TranskribusLogin import TranskribusLogin
+from models.Manuscript import ManuscriptUploading
+from utils.utils_variables import __COLLECTION_ID__
+import os
+
 load_dotenv()
-
 app = FastAPI()
-
-
-class TranskribusAPI:
-    session_id: str
-    session_cookie: dict
-    user: str
-    password: str
-
-    def __init__(self):
-        self.user = os.environ.get("TRANSKRIBUS_USER")
-        self.password = os.environ.get("TRANSKRIBUS_PASSWORD")
-        self.session_cookie = {}
-
-    def post(self, url, data, headers=None):
-        if headers is None:
-            headers = {'Content-Type': 'application/json'}
-        response = requests.post(url, data=urlencode(data), headers=headers,
-                                 cookies=self.session_cookie)
-        return response
-
-    def get(self, url):
-        response = requests.get(url, cookies=self.session_cookie)
-        return response
-
-    def login(self):
-        api_url = "https://transkribus.eu/TrpServer/rest/auth/login"
-        data = {
-            "user": self.user,
-            "pw": self.password
-        }
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        response = self.post(api_url, data, headers)
-        root = ET.fromstring(response.text)
-        self.session_id = root.find("sessionId").text
-        self.session_cookie = {"JSESSIONID": self.session_id}
-        return {"message": "Login was made successfully!", "errors": False}
-
-
-transkribusAPI = TranskribusAPI()
-
-
-# TODO better logging, model view and processes
-class Manuscript(BaseModel):
-    name: str
-    path: str
-    process: object
-    image_bytes: bytes
-    type_of_processing: str
-
-
-# class Recognition(BaseModel):
-#     path_to_file: str
-#     methods: classmethod
-#     processed_output: dict
-#
-#
-# class OCR(Recognition):
-#     pass
-#
-#
-# class HTR(Recognition):
-#     pass
-
-
-class NLP(BaseModel):
-    enhance: bool
-    raw_output: str
-    enhanced_output: str
+transkribusAPI = TranskribusLogin()
+# api_base_url = "https://transkribus.eu/TrpServer/rest/"
+# login_endpoint = "auth/login"
+# collections_endpoint = "collections/list"
+# logout_endpoint = "auth/logout"
+# user = os.environ.get("TRANSKRIBUS_USER")
+# password = os.environ.get("TRANSKRIBUS_PASSWORD")
+# credentials = {'user': user,
+#                'pw': password}
 
 
 @app.get("/")
@@ -85,9 +28,11 @@ async def index():
     return {"message": "Hello World"}
 
 
-@app.post("/receive-manuscript-path/", status_code=201)
-async def receive_path(path: str):
-    return {"path_to_manuscript": path}
+@app.get("/api/v1/upload", status_code=200)
+async def receive_path():
+    manuscript_uploading = ManuscriptUploading(name= "BCUTimișoara689841.jpg",path= "D:\\_ user ecaaa\\Documents\\GitHub\\romanian-script-decipher\\AI-module\\manuscripts_examples\\BCUTimișoara689841.jpg", collection_id= __COLLECTION_ID__)
+    status = manuscript_uploading.upload_manuscript(transkribusAPI.session_id)
+    return status
 
 
 # TODO build process OCR
@@ -112,7 +57,6 @@ async def process_image_htr(path_to_manuscript: str, model_htr):
 def login_to_transkribus():
     response = transkribusAPI.login()
     return response
-
 
 @app.get("/api/v1/third-party/get-collections", status_code=200)
 def get_user_collections():
---
+++ AI-module/models/Manuscript.py
@@ -0,0 +1,67 @@
+import base64
+
+from pydantic import BaseModel
+import requests
+from utils.utils_variables import __URL__, __COLLECTION_ID__
+
+import codecs
+# TODO better logging, model view and processes
+
+
+class ManuscriptUploading:
+    name: str
+    path: str
+    collection_id: int
+
+    def __init__(self, name, path, collection_id):
+        self.name = name
+        self.path = path
+        self.collection_id = collection_id
+
+    # def view_uploads(self,session_id):
+    #     upload_url = __URL__ + f"/uploads"
+
+    def upload_manuscript(self, session_id):
+        upload_url = f"https://transkribus.eu/TrpServer/rest/uploads?collId={self.collection_id}"
+        print(f"collection id is {self.collection_id}")
+        data = {
+            "md": {
+                "title": "BCUTimișoara",
+                "author": "",
+                "genre": "",
+                "writer": ""
+            },
+            "pageList": {"pages": [
+                {
+                    "fileName": self.name,
+                    "pageNr": 1
+                }
+            ]}
+        }
+
+        response = requests.post(upload_url, cookies={"JSESSIONID": session_id}, json=data)
+        if response.status_code == 201 or response.status_code == 200 :
+            print(response.text.split("uploadId>")[1].replace("</", ""))
+            if response.status_code == 201:
+                upload_id = response.json()['uploadId']
+            else:
+                upload_id = int(response.text.split("uploadId>")[1].replace("</", ""))
+            print(f"upload_id is {upload_id}")
+
+            # import chardet
+            #
+            # with open(self.path, 'rb') as file:
+            #     result = chardet.detect(file.read())
+            #     encoding = result['encoding']
+            #
+            # with open(self.path, 'rb') as file:
+            #     content = file.read().decode(encoding)
+            with open(self.path, 'rb') as file:
+                content = file.read()
+                print(content)
+            files = {'img': content}
+            response = requests.put('https://transkribus.eu/TrpServer/rest/uploads/{}'.format(upload_id), files=files)
+            return response
+        else:
+            print(response.status_code)
+            return response.text
