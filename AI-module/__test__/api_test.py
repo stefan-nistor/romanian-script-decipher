d66e9e07836e03fef6c97bf1e4e732ad0270c7b5
TudFilip <filiptudormihail@yahoo.com>
2024-01-15 22:01:53 +0200

    add tests

--- AI-module/__test__/api_test.py
+++ AI-module/__test__/api_test.py
@@ -5,6 +5,6 @@ from main import app
 client = TestClient(app)
 
 def test_login():
-    response = client.get("/third-party/login")
+    response = client.get("/api/v1/third-party/login")
     assert response.status_code == 200
     assert response.json() == {"message": "Login was made successfully!", "errors": False}
